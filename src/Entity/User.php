6ce639fbb5969cd7afcf54bee984091d2117c434
nyc <yve.nguetche@gmail.com>
2022-06-02 15:47:35 +0200

    registration and login form ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -55,6 +56,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->orders = new ArrayCollection();
+        $this->created_at = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,20 +19,73 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('lastName', TextType::class, [
+                'label' => false ,
+                'attr'=> [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'lastname'
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => false ,
+                'attr'=> [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'firstname'
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false ,
+                'attr'=> [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'email'
+                ],
+            ])
+            ->add('address' , TextType::class, [
+                'label' => false ,
+                'attr' => [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'address'
+                ]
+            ])
+            ->add('zipCode' , TextType::class, [
+                'label' => false ,
+                'attr' => [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'zipcode'
+                ]
+            ])
+            ->add('city' , TextType::class, [
+                'label' => false ,
+                'attr' => [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'city'
+                ]
+            ])
+            ->add('country' , TextType::class, [
+                'label' => false ,
+                'attr' => [
+                    'class' => 'form-control-user',
+                    'placeholder' => 'country'
+                ]
+            ])
+            ->add('rgpdConsent', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'You should agree to our RGPD terms.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => false ,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control-user',
+                    'placeholder' => 'password'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
