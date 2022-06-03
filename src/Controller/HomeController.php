fbdc3188ab2ac70921c38221bda9def936d85312
nyc <yve.nguetche@gmail.com>
2022-06-03 17:33:32 +0200

    added product to homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    public function __construct( private ProductRepository $productRepository)
+    {
+        
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $products = $this->productRepository->findAll();
+        return $this->render('home/index.html.twig', compact('products'));
     }
+
+    
 }
