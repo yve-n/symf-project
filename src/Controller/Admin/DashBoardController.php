936e9b2784a7f737a3167004abfa692148aa74da
yve-n <yve.nguetche@gmail.com>
2022-06-28 16:51:57 +0200

    admin dashboard step2

--- src/Controller/Admin/DashBoardController.php
+++ src/Controller/Admin/DashBoardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CategoryRepository;
+use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashBoardController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, 
+    ProductRepository $productRepository, 
+    UserRepository $userRepository,
+    OrderRepository $orderRepository): Response
     {
-        return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'DashBoardController',
-        ]);
+        $categoryCount = $categoryRepository->count([]);
+        $productCount = $productRepository->count([]);
+        $orderCount = $orderRepository->count([]);
+        $userCount = $userRepository->count([]);
+        return $this->render('admin/dashboard/index.html.twig', compact(
+            'categoryCount',
+            'productCount',
+            'orderCount',
+            'userCount'
+            )   
+        );
     }
 }
